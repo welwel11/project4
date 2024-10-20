f16b4ee4eea8a7c8cff3e68c814c8c128f4048ec
welwel11 <dewiyu46@gmail.com>
2024-10-20 18:15:10 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -28,7 +28,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/welwel11/project2/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/welwel11/project4/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
