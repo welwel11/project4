14ac2cb0adc5bfa357b4307595567499d30ed8db
welwel11 <dewiyu46@gmail.com>
2024-10-22 20:12:10 +0700

    Update quota.sh

--- limit/quota.sh
+++ limit/quota.sh
@@ -1,12 +1,12 @@
 REPO="https://raw.githubusercontent.com/welwel11/project4/main/"
-wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit3/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit3/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit3/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}limit3/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}limit3/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}limit3/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}limit3/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit3/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}limit/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}limit/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}limit/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}limit/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit/shadowsocks" >/dev/null 2>&1
 #wget https://raw.githubusercontent.com/welwel11/project1/main/limi3/ip-shadowsocks
 #wget https://raw.githubusercontent.com/welwel11/project1/main/limi3/ip-trojan
 #wget https://raw.githubusercontent.com/welwel11/project1/main/limi3/ip-vless
