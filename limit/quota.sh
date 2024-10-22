1b166dec0137bc080240657c77f83b2c68c179a2
welwel11 <dewiyu46@gmail.com>
2024-10-22 20:06:19 +0700

    Update quota.sh

--- limit/quota.sh
+++ limit/quota.sh
@@ -1,4 +1,4 @@
-REPO="https://raw.githubusercontent.com/welwel11/project1/main/"
+REPO="https://raw.githubusercontent.com/welwel11/project4/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit3/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit3/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit3/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
